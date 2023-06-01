56f0935457d94bc86a33e19999d201046c2d2644
Emissions <unitedrealm@hotmail.com>
2023-06-01 05:28:28 +0100

    updated to ada-events

--- command-handler/src/BaseApp.d.ts
+++ command-handler/src/BaseApp.d.ts
@@ -1,16 +1,16 @@
 import Base from "ts-modular-bot-file-design";
 import { Dependency } from "ada-types";
-import Events from "ts-modular-bot-addon-events-types";
-import DiscordClient from "ts-modular-bot-addon-discord_client-types";
+import Events from "ada-events-types";
+import DiscordClient from "ada-discordclient-types";
 declare abstract class BaseApp extends Base {
     constructor();
     type: Dependency;
     name: string;
     load: boolean;
     static Client: typeof DiscordClient;
-    getClient(): import("ts-modular-bot-addon-discord_client-types/discord-client/src/App").default;
+    getClient(): import("ada-discordclient-types/discord-client/src/App").default;
     static Events: typeof Events;
-    getEvents(): import("ts-modular-bot-addon-events-types/events/src/App").default;
+    getEvents(): import("ada-events-types/events/src/App").default;
     abstract init(): Promise<void>;
     getDependencies(): Dependency[];
 }
